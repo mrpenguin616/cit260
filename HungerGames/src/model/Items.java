9b0871ab08d75e64d1b162519fc8a447c47705a8
Caseywm <mar14057@byui.edu>
2015-05-14 13:12:22 -0600

    Revert "Revert "Fixed some things and removed some things.  ""
    This reverts commit 95d79159b8fd5d4683596bac006befb311365228.

--- HungerGames/src/model/Items.java
+++ HungerGames/src/model/Items.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package byui.cit260.HungerGames;
+package model;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Objects;
 
 /**
