5abc0e8329176563c8b62403f90efc1cff432228
mrpenguin616 <jew12004@byui.edu>
2015-05-26 13:39:27 -0600

    added contoler pkg and some of its classes

---
+++ HungerGames/src/control/GameControl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+import model.Game;
+import model.Owner;
+
+/**
+ *
+ * @author mrpenguin616
+ */
+public class GameControl {
+    public Game gameCreate(){
+         Game game = new Game();
+         return game;
+    }
+    
+    public void gameSave(Game game){
+        // add logic to save game
+        
+    }
+    
+    public Game gameLoad(Owner owner){
+        // add logic to find owner based of owner.name 
+        Game game = new Game(); // will be removed later and changed
+        return game;
+    }
+    
+    public void initMap(){
+    
+    }
+    
+    
+}
---
+++ HungerGames/src/control/mapContol.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+import model.Map;
+import model.Location;
+import model.Player;
+
+/**
+ *
+ * @author mrpenguin616
+ */
+public class mapContol {
+    
+}
